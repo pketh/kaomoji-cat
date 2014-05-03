9e9d145e7c8c3605bd29be9219a47c63ed45bb00
Pirijan <pirijan@gmail.com>
2014-05-03 15:29:22 -0400

    v2 release
    single page app (way faster),
    ios7 updated icon and launch images
    cleaner footer
    clicking cat now does something
    fluid layout (was adaptive)
    upgraded fastclick
    removed 'add this to homescreen' crap

--- README.md
+++ README.md
@@ -1,15 +1,15 @@
 # Kaomoji Cat
 
-Kaomojis are like emojis but in the form of text strings like: 
+Kaomojis are like emojis but in the form of text strings like:
 this happy guy ＼（＠￣∇￣＠）／ <br>
-or 
+or
 this dude flipping over a table (╯°□°）╯︵ ┻━┻
 
-Kaomoji Cat is a webapp that helps you find the perfect kaomoji to express how you're feeling. 
+Kaomoji Cat is a webapp that helps you find the perfect kaomoji to express how you're feeling.
 
 While there are way more kaomojis than emojis, kaomojis can be more flexible and expressive. I played off that quirky expressiveness with the character of the Kaomoji cat, fish and red string. The cat and his app hopefully comes across as cute, precocious and maybe a little bit creepy.
 
-[Try it](http://kaomoji.comoj.com/ "See Kaomoji Cat")<br>
+[Try it](http://kaomojicat.com/ "See Kaomoji Cat")<br>
 [More details](http://pketh.org/Kaomoji-Cat)
 
 ![screenshot](http://payload100.cargocollective.com/1/0/1410/4319041/photo-4.png)
---
+++ public/js/kaomoji-cat.js
@@ -0,0 +1,66 @@
+$(function() {
+
+    console.log('( ﾟ▽ﾟ)/');
+
+    var input = 'js/kaomoji-cat.json'
+    var emoteObj,
+        quoteTemplate,
+        quoteRendered,
+        kaomojisTemplate,
+        kaomojisRendered,
+        listLength;
+
+    render(0);
+
+    function render(emoteIndex) {
+        $.getJSON(input, function(json){
+            emoteObj = json.kaomojicat[emoteIndex];
+
+            quoteTemplate = $('#quote').html();
+            quoteRendered = Mustache.render(quoteTemplate, {'kaomojis': emoteObj});
+            $('.top').detach();
+            $('header').append(quoteRendered);
+
+            kaomojisTemplate = $('#main').html();
+            kaomojisRendered = Mustache.render(kaomojisTemplate, {'kaomojis': emoteObj});
+            $('main section').detach();
+            $('main').append(kaomojisRendered);
+
+            // index page list
+            if (emoteIndex === 0) {
+                listLength = emoteObj.kaomojis.length;
+                for (var i = 1; i <= listLength; i ++ ) {
+                    $('li:nth-child(' + i + ')').wrap('<a href="' + i + '"></a>');
+                }
+            }
+            pushState();
+            backButton();
+        })
+    }
+
+    function pushState() {
+        $('.index a').on('click', function(event) {
+            event.preventDefault();
+            var emote = $(this).attr('href');
+            history.pushState(null, null, emote);
+            render(emote);
+        })
+    }
+
+    function backButton() {
+        $('.cat, nav, .fish').on('click', function() {
+            history.pushState(null, null, '/');
+            render(0);
+        });
+        if ( window.navigator.standalone !== true ) {
+            $('nav').remove();
+        }
+    }
+
+    // browser back
+    window.addEventListener('popstate', function(e) {
+        render(0);
+    });
+
+
+});
